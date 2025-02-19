fd408ed21f203b3a78a3de39a2e65f144a1f6ded
akaman <weils21@mails.tsinghua.edu.cn>
2025-02-19 19:05:30 +0800

    final version:fix a little bug of ready_header_buf

--- src/axi_stream_insert_header.sv
+++ src/axi_stream_insert_header.sv
@@ -28,33 +28,32 @@ module axi_stream_insert_header #(
     input [BYTE_CNT_WD-1 : 0] byte_insert_cnt,
     output ready_insert
 );
-    //data输入端未使用skidbuffer,可以添加
-    logic ready_in_wire;
+    reg ready_in_reg;
 
     //输出端skidbuffer
-    reg valid_out_buf;
-    reg [DATA_WD-1 : 0] data_out_buf;
-    reg [DATA_BYTE_WD-1 : 0] keep_out_buf;
-    reg last_out_buf;
-    wire ready_out_buf;
+    reg valid_out_buf_reg;
+    reg [DATA_WD-1 : 0] data_out_buf_reg;
+    reg [DATA_BYTE_WD-1 : 0] keep_out_buf_reg;
+    reg last_out_buf_reg;
+    wire ready_out_buf_wire;
     skidbuffer #(
 		.DW(DATA_WD+DATA_BYTE_WD+1)
 	) out_buf(
 		.i_clk(clk), .i_reset(rst_n),
-		.i_valid(valid_out_buf), 
-        .o_ready(ready_out_buf),
-		.i_data({data_out_buf, keep_out_buf, last_out_buf}),
+		.i_valid(valid_out_buf_reg), 
+        .o_ready(ready_out_buf_wire),
+		.i_data({data_out_buf_reg, keep_out_buf_reg, last_out_buf_reg}),
 		.o_valid(valid_out), 
         .i_ready(ready_out),
 		.o_data({data_out, keep_out, last_out})
 	);
 
     //header端skidbuffer
-    wire valid_header_buf;
-    wire [DATA_WD-1 : 0] data_header_buf;
-    wire [DATA_BYTE_WD-1 : 0] keep_header_buf;
-    wire [BYTE_CNT_WD-1 : 0] byte_cnt_header_buf;
-    logic ready_header_buf;
+    wire valid_header_buf_wire;
+    wire [DATA_WD-1 : 0] data_header_buf_wire;
+    wire [DATA_BYTE_WD-1 : 0] keep_header_buf_wire;
+    wire [BYTE_CNT_WD-1 : 0] byte_cnt_header_buf_wire;
+    reg ready_header_buf_reg;
     skidbuffer #(
 		.DW(DATA_WD+DATA_BYTE_WD+BYTE_CNT_WD)
     ) header_buf(
@@ -62,9 +61,9 @@ module axi_stream_insert_header #(
 		.i_valid(valid_insert), 
         .o_ready(ready_insert),
 		.i_data({data_insert, keep_insert, byte_insert_cnt}),
-		.o_valid(valid_header_buf), 
-        .i_ready(ready_header_buf&&rst_n),
-		.o_data({data_header_buf, keep_header_buf, byte_cnt_header_buf})
+		.o_valid(valid_header_buf_wire), 
+        .i_ready(ready_header_buf_reg&&rst_n),
+		.o_data({data_header_buf_wire, keep_header_buf_wire, byte_cnt_header_buf_wire})
 	);
 
     
@@ -81,16 +80,16 @@ module axi_stream_insert_header #(
     logic [DATA_WD-1:0] data_to_be_combined_2;
     logic [DATA_BYTE_WD-1:0] keep_to_be_combined_2;
     always_comb begin
-        if((!valid_out_buf&&need_head)||(valid_out_buf&&need_head&&last_out_buf))begin
-            data_to_be_combined_1 = data_header_buf;
-            keep_to_be_combined_1 = keep_header_buf;
+        if((!valid_out_buf_reg&&need_head)||(valid_out_buf_reg&&need_head&&last_out_buf_reg))begin
+            data_to_be_combined_1 = data_header_buf_wire;
+            keep_to_be_combined_1 = keep_header_buf_wire;
         end else begin
             data_to_be_combined_1 = next_head_buf;
             keep_to_be_combined_1 = next_keep_buf;
         end
     end
     always_comb begin
-        if(valid_out_buf&&need_head&&!last_out_buf)begin
+        if(valid_out_buf_reg&&need_head&&!last_out_buf_reg)begin
             data_to_be_combined_2 = '0;
             keep_to_be_combined_2 = '0;
         end else begin
@@ -122,15 +121,15 @@ module axi_stream_insert_header #(
         .combined_keep_2(combined_keep_2)
     );
 
-    //valid_out_buf
+    //valid_out_buf_reg
     always_ff @(posedge clk) begin
         if (~rst_n) begin
-            valid_out_buf <= 1'b0;
+            valid_out_buf_reg <= 1'b0;
         end else begin
             if(ready_in&&valid_in)begin  
-                valid_out_buf <= 1'b1;
-            end else if(valid_out_buf)begin 
-                valid_out_buf <= !ready_out_buf ||(need_head && !last_out_buf);
+                valid_out_buf_reg <= 1'b1;
+            end else if(valid_out_buf_reg)begin 
+                valid_out_buf_reg <= !ready_out_buf_wire ||(need_head && !last_out_buf_reg);
             end
         end
     end
@@ -144,27 +143,27 @@ module axi_stream_insert_header #(
             end 
         end
     end
-    //last_out_buf
+    //last_out_buf_reg
     always_ff @(posedge clk) begin
         if (~rst_n) begin
-            last_out_buf <= 1'b0;
+            last_out_buf_reg <= 1'b0;
         end else begin
-            if(valid_out_buf&&!ready_out_buf)begin
+            if(valid_out_buf_reg&&!ready_out_buf_wire)begin
             end else begin
-                last_out_buf <= !combine_overflow;
+                last_out_buf_reg <= !combine_overflow;
             end
         end
     end
-    //data_out_buf,keep_out_buf
+    //data_out_buf_reg,keep_out_buf_reg
     always_ff @(posedge clk) begin
         if (~rst_n) begin
-            data_out_buf <='0;
-            keep_out_buf <='0;
+            data_out_buf_reg <='0;
+            keep_out_buf_reg <='0;
         end else begin
-            if(valid_out_buf&&!ready_out_buf)begin
+            if(valid_out_buf_reg&&!ready_out_buf_wire)begin
             end else begin
-                data_out_buf <= combined_data_1;
-                keep_out_buf <= combined_keep_1;
+                data_out_buf_reg <= combined_data_1;
+                keep_out_buf_reg <= combined_keep_1;
             end
         end
     end
@@ -174,8 +173,8 @@ module axi_stream_insert_header #(
             next_head_buf <= '0;
             next_keep_buf <= '0;
         end else begin
-            if(valid_out_buf&&!ready_out_buf)begin
-            end else if(!valid_out_buf&&!need_head&&!valid_in)begin
+            if(valid_out_buf_reg&&!ready_out_buf_wire)begin
+            end else if(!valid_out_buf_reg&&!need_head&&!valid_in)begin
             end else begin
                 next_head_buf <= combined_data_2;
                 next_keep_buf <= combined_keep_2;
@@ -185,32 +184,34 @@ module axi_stream_insert_header #(
 
     //ready_in
     always_comb begin
-        if(valid_out_buf)begin
+        if(valid_out_buf_reg)begin
             if(need_head)begin
-                if(last_out_buf)begin
-                    ready_in_wire = ready_out_buf&&valid_header_buf;
+                if(last_out_buf_reg)begin
+                    ready_in_reg = ready_out_buf_wire&&valid_header_buf_wire;
                 end else begin
-                    ready_in_wire = 0;
+                    ready_in_reg = 0;
                 end
             end else begin
-                ready_in_wire = ready_out_buf;
+                ready_in_reg = ready_out_buf_wire;
             end
         end else begin
             if(need_head)begin
-                ready_in_wire = valid_header_buf;
+                ready_in_reg = valid_header_buf_wire;
             end else begin
-                ready_in_wire = 1;
+                ready_in_reg = 1;
             end
         end
     end
-    assign ready_in = ready_in_wire&&rst_n;
+    assign ready_in = ready_in_reg&&rst_n;
 
-    //ready_header_buf
+    //ready_header_buf_reg
     always_comb begin
-        if(!need_head || (valid_header_buf&&!last_out_buf))begin
-            ready_header_buf = 0;
+        if(!need_head || (valid_header_buf_wire&&!last_out_buf_reg))begin
+            ready_header_buf_reg = 0;
+        end else if(valid_out_buf_reg) begin
+            ready_header_buf_reg = valid_in&&ready_out_buf_wire;
         end else begin
-            ready_header_buf = valid_in&&ready_in;
+            ready_header_buf_reg = valid_in;
         end
     end
 
